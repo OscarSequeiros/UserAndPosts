c49df58b883906848b39333b775b26d1364034c5
Oscar Sequeiros <oscar.sequeiros.serrano@gmail.com>
2021-10-15 00:57:25 -0500

    feat: Adding GetAllUsersUseCase

---
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/domain/GetAllUsersUseCase.kt
@@ -0,0 +1,13 @@
+package co.com.ceiba.mobile.pruebadeingreso.domain
+
+import co.com.ceiba.mobile.pruebadeingreso.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetAllUsersUseCase(
+        private val repository: UserRepository
+) {
+
+    operator fun invoke(): Flow<List<User>> {
+        return repository.getAllUsers()
+    }
+}
